62baebdccee360369c356e8695215e1d77b7596e
Mitchell Amihod <mitchell.amihod@gmail.com>
2012-03-18 01:57:15 -0400

    initial commit - fix of example template

---
+++ Classes/ComMeeechPlistModuleAssets.m
@@ -0,0 +1,15 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "ComMeeechPlistModuleAssets.h"
+
+extern NSData * dataWithHexString (NSString * hexString);
+
+@implementation ComMeeechPlistModuleAssets
+
+- (NSData*) moduleAsset
+{
+	return nil;
+}
+
+@end
---
+++ example/app.js
@@ -0,0 +1,24 @@
+// This is a test harness for your module
+// You should do something interesting in this harness 
+// to test out the module and to provide instructions 
+// to users on how to use it by example.
+
+
+// open a single window
+var window = Ti.UI.createWindow({
+	backgroundColor:'white'
+});
+var label = Ti.UI.createLabel();
+window.add(label);
+window.open();
+
+// TODO: write your module tests here
+var plistmodule = require('com.meeech.plist');
+Ti.API.info("module is => " + plistmodule);
+
+label.text = plistmodule.example();
+
+plistmodule.exampleProp = "This is a test value";
+
+Ti.API.info("module exampleProp is => " + plistmodule.exampleProp);
+
